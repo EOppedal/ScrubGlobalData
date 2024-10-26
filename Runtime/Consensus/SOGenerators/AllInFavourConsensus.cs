855edee998423c001c7d09dfee15ffc3764626a2
EOppedal <erlend.oppedal@gmail.com>
2024-10-26 21:24:53 +0200

    rename to fit standard

--- Runtime/Consensus/SOGenerators/AllInFavourConsensus.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq;
-using UnityEngine;
-
-namespace Consensus {
-    [CreateAssetMenu(fileName = nameof(AllInFavourConsensus), menuName = "Scriptable Objects/GlobalData/Consensus/Create " + nameof(AllInFavourConsensus))]
-    public class AllInFavourConsensus : Consensus  {
-        public override bool ConsensusRule() => scriptVotePairs.All(scriptVotePair => scriptVotePair.vote);
-    }
-}
---
+++ Runtime/Consensus/SOGenerators/AllInFavourConsensusSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Consensus {
+    [CreateAssetMenu(fileName = nameof(AllInFavourConsensusSO), menuName = "Scriptable Objects/GlobalData/Consensus/Create " + nameof(AllInFavourConsensusSO))]
+    public class AllInFavourConsensusSO : Consensus  {
+        public override bool ConsensusRule() => scriptVotePairs.All(scriptVotePair => scriptVotePair.vote);
+    }
+}
